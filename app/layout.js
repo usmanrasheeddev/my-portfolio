0b2eff5be8b7a5ea10b99e69c832f97e5699b5f9
usmanrasheed <usmanrasheed.dev@gmail.com>
2026-09-15 01:42:43 +0500

    Use portfolio logo as site favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,10 @@ const dm_sans = DM_Sans({ subsets: ['latin'] })
 export const metadata = {
     title: "Usman Rasheed | Portfolio",
     description: "From code to design, bringing concepts to life with simplicity, style, and passion",
+    icons: {
+        icon: "/assets/logo.png",
+        shortcut: "/assets/logo.png",
+    },
 };
 
 export default function RootLayout({ children }) {
